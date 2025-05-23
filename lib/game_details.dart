cf7ca92e3b4a0bdc37865c016f471f382861bbb9
Kevin N <ken00950@gmail.com>
2025-05-23 18:02:12 -0400

    feat: render badges based on platform for games

--- lib/game_details.dart
+++ lib/game_details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:game_time/game.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -18,6 +19,260 @@ class _GameDetailsState extends State<GameDetails> {
   void initState() {
     super.initState();
     added = false;
+  }
+
+  Widget buildBadge(String platform) {
+    switch (platform) {
+      case 'PlayStation 5':
+        return Container(
+            width: 65,
+            height: 25,
+            padding: EdgeInsets.only(left: 4, right: 4),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(20)),
+            child: SvgPicture.asset(
+              'assets/badges/Playstation 5.svg',
+            ));
+      case 'PlayStation 4':
+        return Container(
+            width: 65,
+            height: 25,
+            padding: EdgeInsets.only(left: 5, right: 5),
+            decoration: BoxDecoration(
+                color: Color.fromARGB(255, 0, 87, 166),
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(20)),
+            child: SvgPicture.asset('assets/badges/Playstation 4.svg',
+                fit: BoxFit.contain));
+      case 'PlayStation 3':
+        return Container(
+            width: 50,
+            height: 25,
+            padding: EdgeInsets.only(left: 5, right: 5),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(20)),
+            child: SvgPicture.asset('assets/badges/Playstation 3.svg',
+                width: 14, height: 15));
+      case 'PlayStation':
+        return Container(
+          width: 55,
+          height: 25,
+          decoration: BoxDecoration(
+            color: Colors.grey.shade400,
+            border: Border.all(color: Colors.grey.shade400),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Row(
+            children: [
+              Container(
+                width: 20, // Adjust the width as needed
+                height: 20, // Adjust the height as needed
+                margin: EdgeInsets.only(left: 6),
+                child: SvgPicture.asset(
+                  'assets/badges/PlayStation.svg',
+                  fit: BoxFit.contain,
+                ),
+              ),
+              const SizedBox(
+                  width: 5), // Adjust the spacing between icon and text
+              const Text(
+                'PS1',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  fontSize: 12, // Adjust the font size as needed
+                ),
+              ),
+            ],
+          ),
+        );
+
+      case 'PlayStation Vita':
+        return Container(
+            width: 65,
+            height: 25,
+            padding: EdgeInsets.only(left: 6, right: 4),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(20)),
+            child: SvgPicture.asset(
+              'assets/badges/Playstation Vita-C.svg',
+            ));
+      case 'Nintendo 64':
+        return Container(
+            width: 50,
+            height: 25,
+            padding: EdgeInsets.only(left: 5, right: 5),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(20)),
+            child: SvgPicture.asset('assets/badges/Nintendo 64.svg',
+                width: 14, height: 15));
+      case 'Wii':
+        return Container(
+            width: 50,
+            height: 25,
+            padding: EdgeInsets.only(left: 5, right: 5),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(20)),
+            child: SvgPicture.asset('assets/badges/Nintendo Wii.svg',
+                width: 14, height: 15));
+      case 'Wii U':
+        return Container(
+            width: 50,
+            height: 25,
+            padding: EdgeInsets.only(left: 5, right: 5),
+            decoration: BoxDecoration(
+                color: Colors.black,
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(20)),
+            child: SvgPicture.asset('assets/badges/Nintendo Wii U.svg',
+                width: 14, height: 15));
+      case 'Xbox 360':
+        return Container(
+            width: 75,
+            height: 25,
+            padding: EdgeInsets.only(left: 5, right: 5),
+            decoration: BoxDecoration(
+                color: Color.fromARGB(255, 226, 228, 229),
+                border: Border.all(color: Colors.red.shade200),
+                borderRadius: BorderRadius.circular(20)),
+            child: SvgPicture.asset('assets/badges/Xbox 360.svg',
+                width: 14, height: 15));
+      case 'Xbox One':
+        return Container(
+            width: 75,
+            height: 25,
+            padding: EdgeInsets.only(left: 5, right: 5),
+            decoration: BoxDecoration(
+                color: Color.fromARGB(154, 6, 106, 16),
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(20)),
+            child: SvgPicture.asset('assets/badges/Xbox One.svg',
+                width: 14, height: 15));
+      case 'Xbox Series X/S':
+        return Container(
+            width: 75,
+            height: 25,
+            padding: EdgeInsets.only(left: 5, right: 5),
+            decoration: BoxDecoration(
+                color: Color.fromARGB(255, 15, 122, 16),
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(20)),
+            child: SvgPicture.asset(
+                'assets/badges/Xbox_Series_X_S-optimized.svg',
+                width: 14,
+                height: 15));
+      case 'PC':
+        return Container(
+          width: 55,
+          height: 25,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.grey.shade400),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Row(
+            children: [
+              Container(
+                width: 20, // Adjust the width as needed
+                height: 20, // Adjust the height as needed
+                margin: EdgeInsets.only(left: 6),
+                child: SvgPicture.asset(
+                  'assets/badges/PC.svg',
+                  fit: BoxFit.contain,
+                ),
+              ),
+              const SizedBox(
+                  width: 5), // Adjust the spacing between icon and text
+              const Text(
+                'PC',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  fontSize: 12, // Adjust the font size as needed
+                ),
+              ),
+            ],
+          ),
+        );
+      case 'Nintendo 3DS':
+        return Container(
+            width: 85,
+            height: 25,
+            padding: EdgeInsets.all(5),
+            decoration: BoxDecoration(
+                color: Colors.white60,
+                border: Border.all(color: Colors.grey.shade400),
+                borderRadius: BorderRadius.circular(20)),
+            child: SvgPicture.asset('assets/badges/Nintendo 3DS.svg',
+                width: 14, height: 150, fit: BoxFit.contain));
+      case 'Nintendo DS':
+        return Container(
+            width: 85,
+            height: 25,
+            padding: EdgeInsets.all(5),
+            decoration: BoxDecoration(
+                color: Colors.grey.shade400,
+                border: Border.all(color: Colors.grey.shade400),
+                borderRadius: BorderRadius.circular(20)),
+            child: SvgPicture.asset('assets/badges/Nintendo DS.svg',
+                width: 14, height: 150, fit: BoxFit.contain));
+      case 'Xbox':
+        return Container(
+          width: 100,
+          height: 100,
+          color: Colors.green, // Example color for Xbox
+          child: Center(child: Text('Xbox')),
+        );
+      case 'Mobile':
+        return Container(
+          width: 65,
+          height: 25,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.grey.shade400),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Row(
+            children: [
+              Container(
+                width: 20, // Adjust the width as needed
+                height: 20, // Adjust the height as needed
+                child: SvgPicture.asset(
+                  'assets/badges/Mobile.svg',
+                  fit: BoxFit.contain,
+                ),
+              ),
+              const Flexible(
+                child: Text(
+                  'Mobile',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 12, // Adjust the font size as needed
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+
+      default:
+        return SizedBox(
+          width: 50,
+          height: 50,
+          child: Text(platform),
+        );
+    }
   }
 
   @override
@@ -251,13 +506,14 @@ class _GameDetailsState extends State<GameDetails> {
                         "Platforms: \n",
                         textAlign: TextAlign.left,
                       ),
-                      Text(
-                        widget.game.platforms.length > 1
-                            ? widget.game.platforms.join(', ')
-                            : widget.game.platforms[0],
-                        overflow: TextOverflow.clip,
-                        maxLines: 4,
-                      ),
+                      if (widget.game.platforms.isNotEmpty)
+                        Wrap(
+                          spacing: 8.0,
+                          runSpacing: 8.0,
+                          children: widget.game.platforms
+                              .map((platform) => buildBadge(platform))
+                              .toList(),
+                        ),
                       const SizedBox(
                         height: 20,
                         width: 340,
